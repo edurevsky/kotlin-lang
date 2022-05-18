85638a3ffe376ceb7702328227eb96c6570e4350
edurevsky <edurevsky@gmail.com>
2022-05-18 17:10:58 -0300

    condicionais com if e when

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,9 +9,24 @@ fun main() {
     var saldo = 0.0
     saldo = 100.0
     saldo += 200
+    saldo -= 400
 
     // Template String
     println("Titular: $titular\n" +
             "Número da conta: $numeroDaConta\n" +
             "Saldo da conta: $saldo")
+
+    if (saldo > 0.0) {
+        println("A Conta é positiva")
+    } else if (saldo == 0.0) {
+        println("A Conta é neutra")
+    } else {
+        println("A Conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("A Conta é positiva")
+        saldo == 0.0 -> println("A Conta é neutra")
+        else -> println("A Conta é negativa")
+    }
 }
