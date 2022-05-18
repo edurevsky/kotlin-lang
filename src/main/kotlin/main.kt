8472bf19218629e25561f5da7e6d7286438218a4
edurevsky <edurevsky@gmail.com>
2022-05-18 17:31:35 -0300

    repetições com for

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,18 +4,23 @@ fun main() {
     // Utilizamos var para indicar que o valor da variável é mutável
     // ou val, onde o seu valor é imutável.
     // Também não precisamos deixar explícito o tipo (int, string) da variável.
-    val titular: String = "João"
-    val numeroDaConta: Int = 4242
-    var saldo = 0.0
-    saldo = 100.0
-    saldo += 200
-    saldo -= 400
 
-    // Template String
-    println("Titular: $titular\n" +
-            "Número da conta: $numeroDaConta\n" +
-            "Saldo da conta: $saldo")
+    for (i in 60 downTo 20 step 20) {
 
+        if (i < 40) break
+
+        val titular: String = "João $i"
+        val numeroDaConta: Int = 4242 + i
+        var saldo = i + 2.0
+
+        // Template String
+        println("Titular: $titular\n" +
+                "Número da conta: $numeroDaConta\n" +
+                "Saldo da conta: $saldo\n")
+    }
+}
+
+fun fazCondicoes(saldo: Double) {
     if (saldo > 0.0) {
         println("A Conta é positiva")
     } else if (saldo == 0.0) {
