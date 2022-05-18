da84ddfe96f07ce44fdd87ee01a705fcf701298b
edurevsky <edurevsky@gmail.com>
2022-05-18 16:45:18 -0300

    variáveis

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main() {
-    print("Bem vindo ao Bytebank!")
+    println("Bem vindo ao Bytebank!")
+
+    // Utilizamos var para indicar que o valor da variável é mutável
+    // ou val, onde o seu valor é imutável.
+    // Também não precisamos deixar explícito o tipo (int, string) da variável.
+    val titular = "João"
+
+    // Template String
+    println("titular $titular")
 }
