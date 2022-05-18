ef1de0d374f45103d2abe34870a833d6fb39e7d2
edurevsky <edurevsky@gmail.com>
2022-05-18 18:14:40 -0300

    classes e objetos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,41 @@
 fun main() {
     println("Bem vindo ao Bytebank!")
 
-    // Utilizamos var para indicar que o valor da variável é mutável
-    // ou val, onde o seu valor é imutável.
-    // Também não precisamos deixar explícito o tipo (int, string) da variável.
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1001
+    contaAlex.saldo = 42.0
 
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1002
+    contaFran.saldo = 300.0
+
+    contaAlex.mostrarDados()
+    contaFran.mostrarDados()
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+
+    fun mostrarDados() {
+        println("--------------------")
+        println("Titular: $titular")
+        println("Número da Conta: $numero")
+        println("Saldo: $saldo")
+        println("--------------------")
+    }
+}
+
+fun fazLacos() {
     for (i in 60 downTo 20 step 20) {
-
         if (i < 40) break
 
+        // Utilizamos var para indicar que o valor da variável é mutável
+        // ou val, onde o seu valor é imutável.
+        // Também não precisamos deixar explícito o tipo (int, string) da variável.
         val titular: String = "João $i"
         val numeroDaConta: Int = 4242 + i
         var saldo = i + 2.0
